d36f917605cfbacb7a04388c4705fc94cb7a6960
Rakkan  Tiasakul <rakkantia@gmail.com>
2017-03-14 09:57:22 +0700

    Add files via upload

---
+++ Terminal.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.util.Scanner;
+import game.Game;
+
+public class Terminal {
+
+	private Game game;
+	private Scanner scanner;
+
+	public void run() {
+		game = new Game();
+		scanner = new Scanner(System.in);
+		if(!game.isEnd()){
+			System.out.println(game.getCurrentPlayerName() + "'s turn");
+			System.out.print("Row: ");
+			int row = scanner.nextInt();
+			System.out.print("Col: ");
+			int col = scanner.nextInt();
+			
+			game.currentPlayerTakesAction(row,col);
+		}
+
+	private void renderBoard(Game game) {
+		int size = game.getBoardSize();
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				String s = game.getSymbolOnBoard(row, col);
+				if(s == null) {
+					s = "_";
+				}
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Terminal ui = new Terminal();
+		ui.run();
+	}
+
+}
